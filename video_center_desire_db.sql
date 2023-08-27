9d25cc75cac8570444ec11931eb6cb27d90f3526
Desire <dndayiragije95@gmail.com>
2023-08-27 22:53:31 +0200

    third commit

--- video_center_desire_db.sql
+++ video_center_desire_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : dim. 27 août 2023 à 20:43
+-- Généré le : dim. 27 août 2023 à 20:51
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.2.0
 
@@ -20,6 +20,85 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `video_center_desire_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+DROP TABLE IF EXISTS `doctrine_migration_versions`;
+CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20230710132200', '2023-07-10 13:24:35', 50),
+('DoctrineMigrations\\Version20230823141235', '2023-08-23 14:14:19', 76),
+('DoctrineMigrations\\Version20230823141855', '2023-08-23 14:19:51', 26),
+('DoctrineMigrations\\Version20230823143848', '2023-08-23 14:40:32', 19),
+('DoctrineMigrations\\Version20230825134151', '2023-08-25 13:43:19', 244),
+('DoctrineMigrations\\Version20230826134006', '2023-08-26 13:42:01', 55),
+('DoctrineMigrations\\Version20230826140325', '2023-08-26 16:04:37', 58),
+('DoctrineMigrations\\Version20230826141256', '2023-08-26 16:14:04', 58);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reset_password_request`
+--
+
+DROP TABLE IF EXISTS `reset_password_request`;
+CREATE TABLE IF NOT EXISTS `reset_password_request` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `selector` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hashed_token` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `requested_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `expires_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  PRIMARY KEY (`id`),
+  KEY `IDX_7CE748AA76ED395` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` json NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '(DC2Type:datetime_immutable)',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '(DC2Type:datetime_immutable)',
+  `is_verified` tinyint(1) NOT NULL,
+  `image_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `image_size` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_1483A5E9E7927C74` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `roles`, `password`, `firstname`, `lastname`, `created_at`, `updated_at`, `is_verified`, `image_name`, `image_size`) VALUES
+(1, 'dndayiragije95@gmail.com', '[]', '$2y$13$yzqnKRQwZI6uyVOfiIDz2eVzMDTjOnQYTopgOEcyHMgN0zkakQyMC', 'NDAYIRAGIJE', 'Désiré', '2023-08-25 11:00:58', '2023-08-27 22:15:39', 1, 'jmbond-64ebaeebce8b4102808237.jpg', 22827),
+(2, 'nkengurutseb7@gmail.com', '[]', '$2y$13$uitJUb0jTdFYCWjXPRZlwOnnDvZSyX1MV8bq06B.j2mAOCxRvz7Am', 'Bienvenu', 'NKENGURUTSE', '2023-08-25 13:16:39', '2023-08-27 21:51:16', 1, 'audibl-64eba934b94e4505552566.avif', 27938),
+(3, 'bizoza20@gmail.com', '[]', '$2y$13$rPyx2mnrL7ZJkIFNtQjOsOp3wmrrHlJLzBf1CkLHHv4D7KTxN9p8u', 'Bizoza', 'James', '2023-08-27 17:21:25', '2023-08-27 19:17:22', 1, 'istockphoto-547218392-1024x1024-64eb852219ec1486025066.jpg', 257816),
+(4, 'kazibe93@gmail.com', '[]', '$2y$13$g8auTgNnVsN7nVcf/xQlPu.e28miFAdQxp4cU0ePFMb2CHn6HonAe', 'Kazibe', 'Joy', '2023-08-27 19:32:00', '2023-08-27 19:39:13', 0, 'image11-64eb8a41ba411145348332.png', 472410);
 
 -- --------------------------------------------------------
 
@@ -71,6 +150,12 @@ INSERT INTO `videos` (`id`, `title`, `video_link`, `description`, `created_at`, 
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `reset_password_request`
+--
+ALTER TABLE `reset_password_request`
+  ADD CONSTRAINT `FK_7CE748AA76ED395` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Contraintes pour la table `videos`
